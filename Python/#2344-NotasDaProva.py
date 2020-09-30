6262f925674d73d1a027dc0f985d2d51c6d18aaa
Jocelino Garcia <39247713+jocelinoFG017@users.noreply.github.com>
2020-09-30 16:36:36 -0300

    Create #2344-NotasDaProva.py

---
+++ Python/#2344-NotasDaProva.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+if (n >= 0) and (n <= 100):
+  if (n == 0):
+    print("E")
+
+  if (n >= 1) and (n <= 35):
+    print("D")
+
+  if (n >= 36) and (n <= 60):
+    print("C")
+
+  if (n >= 61) and (n <= 85):
+    print("B")
+
+  if (n >= 86) and (n <= 100):
+    print("A")
